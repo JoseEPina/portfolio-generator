87af2de6e588cd320f9bfbca242f6ddef3c3a5b1
JoseEPina <jose.edpina@gmail.com>
2021-09-16 22:50:16 -0600

    Added final array methods of filter & map. Also implemented ...rest operator

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
-// const fs = require('fs');
 
 const promptUser = () => {
    return inquirer.prompt([
@@ -64,7 +65,7 @@ Add a New Project
       .prompt([
          {
             type: 'input',
-            name: 'project_name',
+            name: 'projectName',
             message: 'What is the name of your project? (Required)',
             validate: (projectNameInput) => {
                if (projectNameInput) {
@@ -133,7 +134,14 @@ Add a New Project
 promptUser()
    .then(promptProject)
    .then((portfolioData) => {
-      console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+      console.log(pageHTML);
+
+      fs.writeFile('./index.html', pageHTML, (err) => {
+         if (err) throw new Error(err);
+
+         console.log('Page created! Check out index.html in this directory to see it!');
+      });
    });
 
 //#region
